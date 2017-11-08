3885261301a45c0789595f87d41d1a65e4f59942
Erlite <s45405914@sccb.ac.uk>
2017-11-08 18:43:54 +0000

    Create success.sh

---
+++ success.sh
@@ -0,0 +1,2 @@
+echo 'Sending Discord Webhook';
+curl -v -H User-Agent:bot -H Content-Type:application/json -d '{"avatar_url": "https://maxcdn.icons8.com/Share/icon/Logos//travis_ci1600.png", "username": "Travis CI", "embeds": [{"author": {"name": "'"$AUTHOR_NAME"'", "url": "https://github.com/'"$AUTHOR_NAME"'"}, "title": "Commit '"$TRAVIS_COMMIT"': Build Successful!", "type": "rich", "color": 65280, "url": "https://github.com/Erlite/skaiabot-rewrite/commit/'"$TRAVIS_COMMIT"'", "description": "'"$TRAVIS_COMMIT_MESSAGE"'"}]}' $DISCORD_WEBHOOK_URL;
